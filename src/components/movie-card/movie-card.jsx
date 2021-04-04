43eda5ceb24cbe38a384667200f610d2e73eb283
Anton Liverov <liverov@yandex.ru>
2021-04-05 02:15:46 +0300

    Правлю ошибки

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useHistory, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {filmPropType} from '../../types';
 
 import Player from '../player/player';
@@ -7,29 +7,27 @@ import Player from '../player/player';
 const MovieCard = ({film}) => {
   const {itemId, name} = film;
 
-  const history = useHistory();
   const [isPlaying, setIsPlaying] = useState(false);
   return (
-    <>
-      <article className="small-movie-card catalog__movies-card">
-        <Link to={`/films/${itemId}`}>
-          <div
-            onMouseEnter={() => setIsPlaying(true)}
-            onMouseOut={() => setIsPlaying(false)}
-            className="small-movie-card__image"
-          >
-            <Player film={film} isPlaying={isPlaying} isPreviewVideo={true} />
-          </div>
+    <article className="small-movie-card catalog__movies-card">
+      <Link to={`/films/${itemId}`}>
+        <div
+          onMouseEnter={() => setIsPlaying(true)}
+          onMouseOut={() => setIsPlaying(false)}
+          className="small-movie-card__image"
+        >
+          <Player film={film} isPlaying={isPlaying} isPreviewVideo={true} />
+        </div>
+      </Link>
+      <h3 className="small-movie-card__title">
+        <Link
+          to={`/films/${itemId}`}
+          className="small-movie-card__link"
+        >
+          {name}
         </Link>
-        <h3 className="small-movie-card__title">
-          <Link to={`/films/${itemId}`}
-            className="small-movie-card__link"
-          >
-            {name}
-          </Link>
-        </h3>
-      </article>
-    </>
+      </h3>
+    </article>
   );
 };
 
